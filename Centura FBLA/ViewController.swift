f0e15fa3355affdf5318a3fe2e6aec41844f44e0
Colten <crseevers2002@gmail.com>
2019-09-28 23:52:07 -0500

    Initial Commit

---
+++ Centura FBLA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Centura FBLA
+//
+//  Created by Colten.Seevers on 9/28/19.
+//  Copyright © 2019 Colten.Seevers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
